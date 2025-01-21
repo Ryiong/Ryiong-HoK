dbad6c8d43b074e9f91f4a5354f92fbb89ada47c
Ryiong <viethung1209.work@gmail.com>
2025-01-21 11:44:26 +0700

    fix and add file api

--- assets/js/herolist.js
+++ assets/js/herolist.js
@@ -1,36 +1,53 @@
-let path = "https://script.google.com/macros/s/AKfycbz8d-4bGaY45FdNVFy9bGEfv04lxmh1vYi2yf6wElFPeeIZLgGnsjsN-xFBp8BBjWoHhw/exec";
-
-const roleQuery = [
-    "tank",
-    "fighter",
-    "assassin",
-    "mage",
-    "marksmen",
-    "roamer",
-]
-
 const heroListPlace = document.querySelector("#herolist");
 
-function renderList(data) {
-    let text = "";
-    data.forEach(item => {
-        text = text + `<li class="heroitem"><a href="#"><div class="heroblock overflow-hidden"><img class="w-100" src="https://ryiong-hok.neocities.org/assets/Resource/${item.idHero}/${item.idHero}-mobileskin-1.jpg" alt="Hero test"></div><span class="block">${item.nameHero}</span></a></li>`
-    });
-    heroListPlace.innerHTML = text
+function renderList(data, query = "all") {
+  heroListPlace.innerHTML = "";
+  let text = "";
+  data.forEach(item => {
+    text = text + `<li class="heroitem"><a href="#"><div class="heroblock overflow-hidden"><img class="w-100" src="https://ryiong-hok.neocities.org/assets/Resource/${item.idHero}/${item.idHero}-mobileskin-1.jpg" alt="${item.nameHero}"></div><span class="block">${item.nameHero}</span></a></li>`
+  });
+  heroListPlace.innerHTML = text
+  const filterItems = document.querySelectorAll(".filter-item");
+  for (let filterItem of filterItems) {
+    filterItem.classList.remove('active-filter');
+  }
+  target = "#"+query+"hero"
+  document.querySelector(target).classList.add('active-filter')
 }
 
-fetch(path)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data)
-    renderList(data)
-  })
-  .catch((error) => {
-    console.error('There was a problem with the fetch operation:', error)
-    window.location = window.location.href;
-});
+function queryRole(query = '') {
+  if (query == "") {
+    fetch('./assets/json/herolist.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data)
+        renderList(data, "all")
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error)
+      });
+  }
+  else {
+    fetch('./assets/json/herolist'+ query +'.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data)
+        renderList(data, query)
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error)
+      });
+  }
+}
+
+queryRole();
