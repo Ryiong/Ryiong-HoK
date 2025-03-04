112a2d504e75f1cc5bedfb21afd27879ae599b51
Ryiong <viethung1209.work@gmail.com>
2025-03-05 01:09:20 +0700

    banpick layout test product 1

---
+++ match/main.js
@@ -0,0 +1,91 @@
+fetch('./hero.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then((data) => {
+        // console.log(data);
+        RenderListHero(data);
+    })
+    .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error)
+    });
+
+const banBlue = document.querySelector("#ban-blue");
+const banRed = document.querySelector("#ban-red")
+
+function AddSlotBan() {
+    banBlue.innerHTML += `<div class="ban-hero relative" onclick="OpenHeroList(event);"></div>`;
+    banRed.innerHTML += `<div class="ban-hero relative" onclick="OpenHeroList(event);"></div>`
+}
+
+function DeleteSlotBan() {
+    lengthBan = document.querySelectorAll("#ban-blue .ban-hero").length
+    document.querySelectorAll("#ban-blue .ban-hero")[lengthBan - 1].remove();
+    document.querySelectorAll("#ban-red .ban-hero")[0].remove();
+    console.log(lengthBan)
+
+}
+
+function RenderListHero(data) {
+    htmlCode = "";
+    data.forEach(item => {
+        htmlCode += `<div class="hero-profile" data-idhero="${item.idHero}" data-name="${item.nameHero}" onclick="GetHero(event);">
+                    <img src="https://ryiong-hok.neocities.org/assets/Resource/${item.idHero}/${item.idHero}-smallskin-1.jpg" alt="${item.nameHero}">
+                    <span>${item.nameHero}</span>
+                </div>`;
+      });
+    document.querySelector("#list-hero").innerHTML = htmlCode
+}
+
+function CloseHeroList() {
+    document.querySelector("#hero-layout").classList.add("hide")
+    document.querySelector("#search-box").classList.add("hide");
+}
+
+let element = null, textElement = null;
+
+function OpenHeroList(a) {
+    document.querySelector("#hero-layout").classList.remove("hide")
+    document.querySelector("#search-box").classList.remove("hide");
+    if (a.target.classList.contains("ban-hero")) {
+        console.log(a.currentTarget)
+        return [element = a.currentTarget, textElement = null];
+    } else {
+        console.log(a.currentTarget.children[0]);
+        
+        return [element = a.currentTarget.children[0], textElement = a.currentTarget.children[1]];
+    }
+}
+
+function GetHero(e) {
+    const idHero = e.currentTarget.getAttribute("data-idhero");
+    const heroName = e.currentTarget.getAttribute("data-name");
+    console.log(idHero)
+    
+    element.style.background = `url('https://ryiong-hok.neocities.org/assets/EsportsUI/${idHero}.jpg') center / cover no-repeat`;
+    if (textElement) {
+        textElement.innerText = heroName;
+    }
+}
+
+function SearchHero() {
+    let query = document.getElementById("search-input").value.toLowerCase();
+    let heroes = document.querySelectorAll(".hero-profile");
+    heroes.forEach(hero => {
+        let name = hero.getAttribute("data-name").toLowerCase();
+        if (name.includes(query)) {
+            hero.classList.remove("hide")
+        } else {
+            hero.classList.add("hide")
+        }
+    })
+}
+
+function handleKeyPress(event) {
+    if (event.key === "Enter") {
+        SearchHero();
+    }
+}
